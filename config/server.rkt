522fff91918b0bc1916f6369039266b7cf139bdd
Matt Jadud <jadudm@gmail.com>
2012-07-15 21:25:00 -0400

    Setting everything to version 100.

--- config/server.rkt
+++ config/server.rkt
@@ -2,7 +2,7 @@
       (server-port . 8000)
       (base-url . "http://localhost:8000")
       (remote-url . "https://raw.github.com/craftofelectronics/flow/master")
-      (versions . (("server.rkt"      "config"        99)
+      (versions . (("server.rkt"      "config"       100)
                    ("arduino.rkt"     "config"       100)
                    ("arduinouno.rkt"  "config"       100)
                    ("flow.module"     "occam/flow"   100)))
