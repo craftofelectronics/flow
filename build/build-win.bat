798a5de4850db83f5eeb436efea9cbfea2a5c5c1
Matthew Jadud <matt@jadud.com>
2012-07-19 20:56:13 -0400

    Adding in the start of a Windows build script.
    And, I have command-line git for Windows. Life just got better.
    
    It included VIM.

---
+++ build/build-win.bat
@@ -0,0 +1,38 @@
+@ECHO OFF
+
+echo. Setting up variables
+
+set src=C:\Documents and Settings\Administrator\My Documents\GitHub\flow
+set build=%src%\build
+set dest=%src%\build\flow
+set raco="C:\Program Files\Racket\raco.exe"
+
+REM MAKE DESTINATION DIRECTORY
+echo. Making destination directories.
+mkdir "%dest%"
+REM MAKE TEMP DIRECTORY IN DESTINATION
+mkdir "%dest%\temp"
+
+REM BUILD EXECUTABLE
+echo. Building executable.
+cd %build%
+%raco% exe -o flow.exe "%src%\server\gui.rkt"
+
+REM MAKE IT DISTRIBUTABLE
+echo. Making executable distributable.
+%raco% distribute "%dest%" flow.exe
+
+REM COPY NEEDED DIRECTORIES
+call:xcopy_sd bin
+call:xcopy_sd interface
+call:xcopy_sd occam
+GOTO:EOF
+
+
+REM End of script
+goto:eof
+
+:xcopy_sd
+  echo. Copying %1
+  xcopy /S /F /Y /I "%src%\%1" "%dest%\%1"
+GOTO:EOF
