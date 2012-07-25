526c0c0d575db74f9fb91ca775bb1c895f272c64
Matt Jadud <jadudm@gmail.com>
2012-07-25 17:13:44 -0400

    Adding another file to the updates...

--- config/server.rkt
+++ config/server.rkt
@@ -5,9 +5,13 @@
       (versions . (("server.rkt"      "config"       100)
                    ("arduino.rkt"     "config"       100)
                    ("arduinouno.rkt"  "config"       100)
-                   ("flow.module"     "occam/flow"   101)))
+                   ("flow.module"     "occam/flow"   101)
+                   ("ifttt.js"        "interface"    100)
+                   ))
       (update-paths . (("occam/flow"
                         "https://raw.github.com/craftofelectronics/flow/master/occam/flow/")
+                       ("interface"
+                        "https://raw.github.com/craftofelectronics/flow/master/interface/")
                        ("config"
                         "https://raw.github.com/craftofelectronics/flow/master/config/")))
       (max-windows-com-port . 15)
